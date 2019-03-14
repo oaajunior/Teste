519d96c518826b424d1ffeadc9d53d80a6054c68
“Oberdan” <oaajunior@gmail.com>
2019-03-14 14:18:35 -0300

    Primeira versao do projeto

---
+++ pad12c20190307/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pad12c20190307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
